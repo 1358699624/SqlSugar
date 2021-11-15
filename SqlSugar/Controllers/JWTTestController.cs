75c301f54e3473738a16498ae4459ef4b5bf6a22
1358699624@qq.com <1358699624@qq.com>
2021-11-15 17:38:10 +0800

    控制器调用API接口测试

--- SqlSugar/Controllers/JWTTestController.cs
+++ SqlSugar/Controllers/JWTTestController.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyBlog.Model;
+using MySqlSugar.Utility.ApiResult;
 using MySqlSugar.Utility.Filter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MySqlSugar.Controllers
@@ -41,5 +46,82 @@ namespace MySqlSugar.Controllers
             } );
         }
 
+        [AllowAnonymous]
+        [HttpGet("GetApiAsync")]
+        public async Task<ActionResult<ApiResult>> GetApiAsync(string path)//调用API，Get请求，无参
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync(path);
+                string msg="";
+                if (response.IsSuccessStatusCode)
+                {
+                    msg = await response.Content.ReadAsAsync<string>();
+                }
+                return ApiResultHelper.Success(msg);
+            }
+            catch (Exception ex)
+            {
+                return ApiResultHelper.Error(ex.Message);
+            }
+        }
+
+
+
+        [AllowAnonymous]
+        [HttpGet("GetApiAsync2")]
+        public async Task<ActionResult<ApiResult>> GetApiAsync2(string path= "https://api.github.com/orgs/dotnet/repos")//调用API，Get请求，有参
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var  stream = await client.GetStreamAsync(path);
+                var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+                return ApiResultHelper.Success(repositories);
+            }
+            catch (Exception ex)
+            {
+                return ApiResultHelper.Error(ex.Message);
+            }
+        }
+
+        
+
+        [AllowAnonymous]
+        [HttpGet("PostApiAsync")]
+        public async Task<ActionResult<ApiResult>> PostApiAsync(string path, string name)//调用API，Post请求，有参
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //client.BaseAddress = new Uri(path);
+                //UserInfo userInfo = null;
+                HttpResponseMessage response = await client.GetAsync(path);
+                string msg = "";
+                if (response.IsSuccessStatusCode)
+                {
+                    msg = await response.Content.ReadAsAsync<string>();
+                }
+                return ApiResultHelper.Success(msg);
+            }
+            catch (Exception ex)
+            {
+                return ApiResultHelper.Error(ex.Message);
+            }
+        }
+    }
+    public class Repository
+    {
+        public string name { get; set; }
     }
 }
+ 
---
+++ SqlSugar/Utility/HttpClientHelper.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySqlSugar.Utility.ApiResult;
+
+namespace MySqlSugar.Utility
+{
+    public  class HttpClientHelper
+    {
+
+            private static readonly HttpClient HttpClient;
+
+            static HttpClientHelper()
+            {
+                var handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.None };
+                HttpClient = new HttpClient(handler);
+            }
+
+
+            #region GET
+
+            /// <summary>
+            /// get请求，可以对请求头进行多项设置
+            /// </summary>
+            /// <param name="url"></param>
+            /// <returns></returns>
+            public static string GetResponseByGet(string url)
+            {
+                return GetResponseByGet(url, null);
+            }
+            /// <summary>
+            /// get请求，可以对请求头进行多项设置
+            /// </summary>
+            /// <param name="url"></param>
+            /// <param name="paramArray"></param>
+            /// <returns></returns>
+            public static string GetResponseByGet(string url, List<KeyValuePair<string, string>> paramArray)
+            {
+                string result = "";
+
+                if (paramArray != null)
+                    url = url + "?" + BuildParam(paramArray);
+
+                var httpclient = HttpClientHelper.HttpClient;
+                var response = httpclient.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Stream myResponseStream = response.Content.ReadAsStreamAsync().Result;
+                    StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                    result = myStreamReader.ReadToEnd();
+                    myStreamReader.Close();
+                    myResponseStream.Close();
+                }
+
+                return result;
+            }
+
+
+            public static string GetResponseBySimpleGet(string Url)
+            {
+                return GetResponseBySimpleGet(Url, null);
+            }
+            public static string GetResponseBySimpleGet(string Url, List<KeyValuePair<string, string>> paramArray)
+            {
+                string result = string.Empty;
+
+                var httpclient = HttpClientHelper.HttpClient;
+
+                if (paramArray != null)
+                    Url = Url + "?" + BuildParam(paramArray);
+
+                //var result = httpclient.GetStringAsync(Url).Result;
+
+                try
+                {
+                    using (HttpClient http = new HttpClient())
+                    {
+
+                        HttpResponseMessage message = null;
+
+                        http.DefaultRequestHeaders.Add("Bearer", "");
+
+                        var task = http.GetAsync(Url);
+                        message = task.Result;
+
+                        if (message != null && message.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            using (message)
+                            {
+                                result = message.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return result;
+            }
+
+
+            #endregion
+
+            #region POST
+
+            public static string HttpPostRequestAsync(string Url, List<KeyValuePair<string, string>> paramArray, string ContentType = "application/json")
+            {
+                return HttpPostRequestAsync(Url, paramArray, null, ContentType);
+            }
+
+            public static string HttpPostRequestAsync(string Url, object objBody, string ContentType = "application/json")
+            {
+                return HttpPostRequestAsync(Url, null, objBody, ContentType);
+            }
+
+            public static string HttpPostRequestAsync(string Url, List<KeyValuePair<string, string>> paramArray, object objBody, string ContentType = "application/x-www-form-urlencoded")
+            {
+                string result = "";
+
+                var postData = BuildParam(paramArray);
+                string strBody = string.Empty;
+
+                if (paramArray != null)
+                    Url = string.Concat(new string[] { Url, "?", postData });
+
+                if (objBody != null)
+                   
+                    strBody = objBody as string;
+
+            try
+                {
+                    using (HttpClient http = new HttpClient())
+                    {
+ 
+                        HttpResponseMessage message = null;
+
+                        using (StringContent content = new StringContent(strBody, Encoding.UTF8, ContentType))
+                        {
+                            http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ContentType));
+
+                            var task = http.PostAsync(Url, content);
+                            message = task.Result;
+                        }
+
+                        if (message != null && message.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            using (message)
+                            {
+                                result = message.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                return result;
+            }
+
+
+
+            #endregion
+
+            private static string Encode(string content, Encoding encode = null)
+            {
+                if (encode == null) return content;
+
+                return System.Web.HttpUtility.UrlEncode(content, Encoding.UTF8);
+
+            }
+
+            private static string BuildParam(List<KeyValuePair<string, string>> paramArray, Encoding encode = null)
+            {
+                string url = "";
+
+                if (encode == null) encode = Encoding.UTF8;
+
+                if (paramArray != null && paramArray.Count > 0)
+                {
+                    var parms = "";
+                    foreach (var item in paramArray)
+                    {
+                        parms += string.Format("{0}={1}&", Encode(item.Key, encode), Encode(item.Value, encode));
+                    }
+                    if (parms != "")
+                    {
+                        parms = parms.TrimEnd('&');
+                    }
+                    url += parms;
+
+                }
+                return url;
+            }
+
+        
+
+    }
+}
